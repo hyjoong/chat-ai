3649a2e358fbcd0c6ebecb24d524896f223b6109
henry <70426440+hyjoong@users.noreply.github.com>
2023-04-13 22:51:54 +0900

    feat: layout component moblie image 적용

--- components/common/Layout/Layout.styles.ts
+++ components/common/Layout/Layout.styles.ts
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: relative;
   width: 500px;
   height: 100vh;
-  background-color: ${({ theme }) => theme.colors.black900};
-  padding: 0 25px;
+  padding: 100px 60px;
   margin: 0 auto;
+  /* background-color: black; */
+`;
+
+export const BackgroundImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  object-fit: cover;
+`;
+
+export const Content = styled.div`
+  position: relative;
+  z-index: 1;
+  height: 100%;
 `;
--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -2,7 +2,15 @@ import React, { PropsWithChildren } from 'react';
 import * as S from './Layout.styles';
 
 const Layout = ({ children }: PropsWithChildren) => {
-  return <S.Container>{children}</S.Container>;
+  return (
+    <S.Container>
+      <S.BackgroundImage
+        src="images/mobile-background.png"
+        alt="Mobile background"
+      />
+      <S.Content>{children}</S.Content>
+    </S.Container>
+  );
 };
 
 export default Layout;
