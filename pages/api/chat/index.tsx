d7bab338fb7c9ad8c6258d1e4bf7b730cb36de33
henry <70426440+hyjoong@users.noreply.github.com>
2023-04-23 16:35:24 +0900

    feat: chat api 로직 수정 - using gpt-3.5-turbo - setting role: 'system' - lengthmax_tokens error handling

--- pages/api/chat/index.tsx
+++ pages/api/chat/index.tsx
@@ -1,29 +1,40 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
+import { OpenAIApi, Configuration } from 'openai';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { sentence, apiKey } = req.query;
+  const { sentence, apiKey, roomCount } = req.query;
 
   try {
-    const response = await fetch('https://api.openai.com/v1/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + apiKey,
-      },
-      body: JSON.stringify({
-        model: 'text-davinci-003',
-        prompt: sentence,
-        max_tokens: 200,
-      }),
+    const configuration = new Configuration({
+      apiKey: apiKey as string,
     });
-    const { choices } = await response.json();
-    const data = choices[0].text;
+    const aiBotCount = Number(roomCount) - 1;
+    const openai = new OpenAIApi(configuration);
+    const { data } = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      max_tokens: 128,
+      messages: [
+        {
+          role: 'system',
+          content: `This is a chat room where one user and ${aiBotCount} AI are chatting. Please feel free to provide responses, ask new questions, or change the topic within 3 lines.`,
+        },
+        { role: 'user', content: sentence as string },
+      ],
+    });
+
+    if (data.choices[0].finish_reason === 'lengthmax_tokens') {
+      res.status(401).json({
+        success: false,
+        message:
+          'API key usage limit exceeded. Please log in with a different API key',
+      });
+    }
+
     res.status(200).json({
       success: true,
       message: 'Success in getting chatting data',
-      data,
+      data: data.choices[0],
     });
-  } catch (error) {
+  } catch (error: any) {
     res.status(400).json({ error });
   }
 };
