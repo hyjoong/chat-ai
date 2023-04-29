c28844edae51bb6089190888a50c68bc4e0c16fc
henry <70426440+hyjoong@users.noreply.github.com>
2023-04-29 13:06:24 +0900

    feat: useAuth customhook getOpenApiKey 함수 적용

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { getOpenApiKey } from 'storage/service';
 
 export const useAuth = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const API_KEY = localStorage.getItem('apiKey');
+    const API_KEY = getOpenApiKey();
 
     if (!API_KEY) {
       router.push('/login');
